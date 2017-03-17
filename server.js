178a6eafd439a6bb6836be0be80771bb4139176d
Maciej Gorczynski <mgorczynski2@ibahealth.local>
2017-03-17 08:52:57 +0100

    adding primeng

--- server.js
+++ server.js
@@ -5,14 +5,6 @@ const app = express();
 // Run the app by serving the static files
 // in the dist directory
 app.use(express.static(__dirname + '/dist'));
-// Start the app by listening on the default
-// Heroku port
-// For all GET requests, send back index.html
-
-// If an incoming request uses
-// a protocol other than HTTPS,
-// redirect that request to the
-// same url but with HTTPS
 
 // so that PathLocationStrategy can be used
 app.get('/*', function(req, res) {
